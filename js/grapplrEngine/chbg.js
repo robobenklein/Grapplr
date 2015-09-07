69984a580f9460f71446c60bc13eedb14f9f2804
PressOnThis <planetpresto@gmail.com>
2015-09-07 19:35:22 -0400

    Fix Errors in chbg

--- js/grapplrEngine/chbg.js
+++ js/grapplrEngine/chbg.js
@@ -1,11 +1,11 @@
 function readURL(input) {
-
         if (input.files && input.files[0]) {
             var reader = new FileReader();
             reader.onload = function (e) {
                 var backgroundImage = "url('" + e.target.result + "')";
                 document.body.style.backgroundImage = backgroundImage;
                 localStorage.setItem("imgData", backgroundImage);
+                window.location.replace("grapplr.html");
             }
             reader.readAsDataURL(input.files[0]);
         }
@@ -13,3 +13,7 @@ function readURL(input) {
     document.getElementById("changebg").onchange = function(){
         readURL(this);
     }
+window.onerror = function(){
+    alert("your background image is too large to save. Try another one instead! :)");
+     document.getElementById("load").style.display = "none";
+}
